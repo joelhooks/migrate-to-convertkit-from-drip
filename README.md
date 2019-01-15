11aa73def436374f35065aae851ee1b3fbd97ad6
Joel Hooks <joel@egghead.io>
2019-01-15 13:15:40 -0800

    hello git

---
+++ README.md
@@ -0,0 +1,1 @@
+simple script to read a drip export and convert it to a csv for each tag.
---
+++ index.js
@@ -0,0 +1,32 @@
+const _ = require('lodash')
+const fs = require('fs')
+const csv = require('csv-parser')
+const writer = require('fast-csv')
+
+const csvFilePath = './subscribers.csv'
+const tagSep = ','
+const filterFor = '' // only looking for tags with a specific string
+const filesToBuild = {}
+
+fs.createReadStream(csvFilePath)
+  .pipe(csv())
+  .on('data', function(row) {
+    row.tags = _.filter(row.tags.split(tagSep), tag => tag.includes(filterFor))
+    row.tags.forEach(tag => {
+      const {tags, ...props} = row
+      const trimmedTag = _.trim(tag)
+      //each tag will get its own csv output, so add it to the hash
+      filesToBuild[trimmedTag] = filesToBuild[trimmedTag] || []
+      filesToBuild[trimmedTag].push({...props})
+    })
+  })
+  .on('end', function() {
+    _.keys(filesToBuild).forEach(key => {
+      //go through all the filtered tags and write a csv file with the appropriate rows
+      writer
+        .writeToPath(`./${key}.csv`, filesToBuild[key], {headers: true})
+        .on('finish', function() {
+          console.log(`wrote ${key}.csv`)
+        })
+    })
+  })
